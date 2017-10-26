eec48389d7d225275497d89e4164dc4a5264c173
stijn-PC\stijn <stijnkluiters@hotmail.com>
2017-10-26 18:01:35 +0200

    First commit

---
+++ authentication_functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stijn
+ * Date: 26-10-2017
+ * Time: 17:48
+ */
+
+function check_password($hashedOriginalPassword, $input)
+{
+    $options = ['cost' => 15];
+
+    if(password_verify($input, $hashedOriginalPassword)){
+        if (password_needs_rehash($hashedOriginalPassword, PASSWORD_DEFAULT,  $options)) {
+            // If so, create a new hash, and replace the old one
+            // TODO: how to know if user had an strong or default password?
+            $newHash = generatePassword($input, false);
+            // TODO: save newHash to the user
+        }
+    };
+}
+function generatePassword($input, $highSecurity = false)
+{
+    $options = ['cost' => 15];
+    if($highSecurity) {
+        return password_hash($input, PASSWORD_BCRYPT, $options);
+    } else {
+        return password_hash($input, PASSWORD_DEFAULT, $options);
+    }
+}
