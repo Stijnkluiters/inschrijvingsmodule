8da516dc7b5ee0674ff42d88b2b1422525b35f9a
Johan Vd Wetering <s1119240@student.windesheim.nl>
2017-11-21 11:49:28 +0100

    Add table

--- beheerder/gebruikers/edit_leerling.php
+++ beheerder/gebruikers/edit_leerling.php
@@ -5,5 +5,22 @@
  * Date: 21-11-2017
  * Time: 11:11
  */
+?>
+<table>
+    <tr>
+        <td>Nummer</td>
+        <td>Geslacht</td>
+        <td>Roepnaam</td>
+        <td>Voorvoegsel</td>
+        <td>Achternaam</td>
+        <td>Opleiding</td>
+        <td>Geboortedatum</td>
+        <td>Postcode en Plaats</td>
+        <td>Begindatum</td>
+        <td>Eindedatum</td>
+        <td>Actie</td>
+    </tr>
+    <tr>
 
-print("Hello");
+    </tr>
+</table>
