2d14378616a6d369106526b2e53e36de6bd31480
Johan Vd Wetering <s1119240@student.windesheim.nl>
2017-11-21 11:53:34 +0100

    edit TD into TH

--- beheerder/gebruikers/edit_leerling.php
+++ beheerder/gebruikers/edit_leerling.php
@@ -8,17 +8,17 @@
 ?>
 <table style="width:100%" border: 1px solid black>
     <tr>
-        <td>Nummer</td>
-        <td>Geslacht</td>
-        <td>Roepnaam</td>
-        <td>Voorvoegsel</td>
-        <td>Achternaam</td>
-        <td>Opleiding</td>
-        <td>Geboortedatum</td>
-        <td>Postcode en Plaats</td>
-        <td>Begindatum</td>
-        <td>Eindedatum</td>
-        <td>Actie</td>
+        <th>Nummer</th>
+        <th>Geslacht</th>
+        <th>Roepnaam</th>
+        <th>Voorvoegsel</th>
+        <th>Achternaam</th>
+        <th>Opleiding</th>
+        <th>Geboortedatum</th>
+        <th>Postcode en Plaats</th>
+        <th>Begindatum</th>
+        <th>Eindedatum</th>
+        <th>Actie</th>
     </tr>
     <tr>
 
