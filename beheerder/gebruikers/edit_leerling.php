f8c79a235eec2e96f994d7a3c0f144465f6d087d
Johan Vd Wetering <s1119240@student.windesheim.nl>
2017-11-21 14:47:16 +0100

    add new table

--- beheerder/gebruikers/edit_leerling.php
+++ beheerder/gebruikers/edit_leerling.php
@@ -27,11 +27,47 @@ $docenten = $docentenQuery->fetchAll();
         <th>Actie</th>
     </tr>
     </thead>
-    <tr>
-        <td>
-            <?php
+    <tr><?php
+        class TableRows extends RecursiveIteratorIterator {
+        function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+        }
 
-            ?>
-        </td>
+        function current() {
+        return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
+        }
+
+        function beginChildren() {
+        echo "<tr>";
+        }
+
+        function endChildren() {
+        echo "</tr>" . "\n";
+    }
+    }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "inschrijfmodule";
+
+    try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, roepnaam, achternaam FROM gebruiker");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+    }
+    }
+    catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+    echo "</table>";?>
     </tr>
 </table>
