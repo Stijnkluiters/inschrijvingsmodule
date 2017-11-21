920b9a5402742f4846f19e24c0139deb60198e77
stijn-PC\stijn <stijnkluiters@hotmail.com>
2017-11-21 16:09:15 +0100

    - patched overview docenten

--- beheerder/gebruikers/edit_docent.php
+++ beheerder/gebruikers/edit_docent.php
@@ -9,66 +9,48 @@
 /**Select * from gebruiker where afkorting is not null*/
 
 $db = db();
-$docentenQuery = $db->prepare('SELECT afkorting, roepnaam, voorvoegsel, achternaam, geslacht, geboortedatum  FROM gebruiker WHERE afkorting IS NOT NULL');
+$docentenQuery = $db->prepare('SELECT 
+g.afkorting as afkorting, 
+g.roepnaam  as roepnaam, 
+g.voorvoegsel as voorvoegsel, 
+g.achternaam as achternaam, 
+g.geslacht as geslacht, 
+g.geboortedatum as geboortedatum  
+FROM gebruiker g 
+JOIN gebruiker_heeft_rol gr ON g.id = gr.gebruiker_id
+JOIN rol r ON r.id = gr.rol_id
+WHERE r.naam = "docent" ');
+
 $docentenQuery->execute();
 $docenten = $docentenQuery->fetchAll();
 
 ?>
 <table class="table table-bordered">
     <thead class="thead-dark">
-    <tr>
-        <th>afkorting</th>
-        <th>Roepnaam</th>
-        <th>Voorvoegsel</th>
-        <th>Achternaam</th>
-        <th>Geslacht</th>
-        <th>Geboortedatum</th>
-
-    </tr>
+        <tr>
+            <th>afkorting</th>
+            <th>Roepnaam</th>
+            <th>Voorvoegsel</th>
+            <th>Achternaam</th>
+            <th>Geslacht</th>
+            <th>Geboortedatum</th>
+        </tr>
     </thead>
-    <tr><?php
-        class TableRows extends RecursiveIteratorIterator {
-            function __construct($it) {
-                parent::__construct($it, self::LEAVES_ONLY);
-            }
-
-            function current() {
-                return "<td style='width: 150px; border: 1px solid black;'>" . parent::current(). "</td>";
-            }
-
-            function beginChildren() {
-                echo "<tr>";
-            }
-
-            function endChildren() {
-                echo "</tr>" . "\n";
-            }
-        }
-
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "inschrijfmodule";
-
-        try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $stmt = $conn->prepare("SELECT afkorting, roepnaam, voorvoegsel, achternaam, geslacht, geboortedatum  FROM gebruiker WHERE afkorting IS NOT NULL");
-            $stmt->execute();
-
-            // set the resulting array to associative
-            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-
-            foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-                echo $v;
-            }
-        }
-        catch(PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-        $conn = null;
-        echo "</table>";?>
+    <?php
+    foreach ($docenten as $docent)
+    {
+    ?>
+    <tr>
+        <td><?= $docent['afkorting'] ?></td>
+        <td><?= $docent['roepnaam'] ?></td>
+        <td><?= $docent['voorvoegsel'] ?></td>
+        <td><?= $docent['achternaam'] ?></td>
+        <td><?= $docent['geslacht'] ?></td>
+        <td><?= $docent['geboortedatum'] ?></td>
     </tr>
+    <?php
+    }
+    ?>
 </table>
 
 
