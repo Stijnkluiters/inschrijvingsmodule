88d12c42690f97cd4e70461a8391df034706ac5c
Jonas <jonaskalkman@gmail.com>
2017-11-22 10:14:42 +0100

    vrijwel alle info laten zien in een tabel fetchAll veranderd in constructie met alleen fetch

--- evenement/bekijken.php
+++ evenement/bekijken.php
@@ -8,18 +8,28 @@
 <?php
 $db = db();
 
-$stmt = $db->prepare("SELECT e.titel, es.onderwerp, e.datum, e.adres_id, e.locatie_id FROM evenement e JOIN evenement_soort es ON evenement_soort_id = es.id");
-
+$stmt = $db->prepare("SELECT e.titel, es.onderwerp, e.datum, e.adres_id, e.locatie_id FROM evenement e JOIN evenement_soort es ON evenement_soort_id = es.id LEFT JOIN locatie l ON l.id = e.locatie_id LEFT JOIN adres a ON a.id = e.adres_id");
 $stmt->execute();
-$results = $stmt->fetchAll();
-
 ?>
-<table>
+<div>
+    <table>
     <?php
-    foreach ($results as $result) {
-        var_dump($result);
-    }
+
+    while ($row = $stmt->fetch()) {
+        $adres = "n.v.t.";
+        $locatie = "n.v.t.";
+        $titel = $row["titel"];
+        $onderwerp = $row["onderwerp"];
+        $datum = $row["datum"];
+        if($row["adres_id"] != "") {
+            $adres = $row["adres_id"];
+        }
+        if($row["locatie_id"] != ""){
+            $locatie = $row["locatie_id"];
+        }
+    print("<tr><td>$titel</td><td> Onderwerp: $onderwerp</td><td> Datum: $datum</td><td> Adres: $adres</td><td> Locatie: $locatie</td></tr>");}
     ?>
-</table>
+    </table>
+</div>
 </body>
 </html>
