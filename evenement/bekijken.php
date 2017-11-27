33d8621da8068f9ba44b665f460d94a6ba77bc2e
Jonas <jonaskalkman@gmail.com>
2017-11-27 18:11:36 +0100

    evenementen bekijken af, include bij index.php geeft problemen. specifiek.php laadt niet

--- evenement/bekijken.php
+++ evenement/bekijken.php
@@ -1,6 +1,8 @@
 <?php
+//load database
 $db = db();
 
+//take info from database/evenement
 $stmt = $db->prepare("
 SELECT e.id, e.titel, es.onderwerp, e.datum, e.adres_id, e.locatie_id 
 FROM evenement e JOIN evenement_soort es 
@@ -8,43 +10,59 @@ ON evenement_soort_id = es.id
 LEFT JOIN locatie l ON l.id = e.locatie_id 
 LEFT JOIN adres a ON a.id = e.adres_id");
 $stmt->execute();
-?>
-<div class="allEvents">
-    <table>
-        <tr>
-            <th id="titel" class="tablehead">naam</th>
-            <th class="tablehead">onderwerp</th>
-            <th class="tablehead">datum</th>
-            <th class="tablehead">plaats</th>
-            <th class="tablehead">locatie</th>
-        </tr>
-    <?php
-    while ($row = $stmt->fetch()) {
-        $id = $row["id"];
+
+//get results from query
+$rows = $stmt->fetchAll();
+
+//check for content in results (12 field per row, so divide by 12)
+$countrow = count($rows);
+if(count($countrow) > 0) {
+
+    //set up base for table
+    print('
+        <div class="allEvents">
+        <table>
+            <tr>
+                <th id="titel" class="tablehead">naam</th>
+                <th class="tablehead">onderwerp</th>
+                <th class="tablehead">datum</th>
+                <th class="tablehead">plaats</th>
+                <th class="tablehead">locatie</th>
+            </tr>
+        ');
+    //create a loop to get the needed info per part
+
+    foreach ($rows as $row){
+
+        htmlentities($row["id"]);
         $adres = "n.v.t.";
         $locatie = "n.v.t.";
-        $titel = $row["titel"];
-        $onderwerp = $row["onderwerp"];
-        $datum = $row["datum"];
-        if($row["adres_id"] != "") {
-            $adres = $row["adres_id"];
+        htmlentities($row["titel"]);
+        htmlentities($row["onderwerp"]);
+        htmlentities($row["datum"]);
+        if ($row["adres_id"] != "") {
+            $adres = htmlentities($row["adres_id"]);
         }
-        if($row["locatie_id"] != ""){
-            $locatie = $row["locatie_id"];
+        if ($row["locatie_id"] != "") {
+            $locatie = htmlentities($row["locatie_id"]);
         }
-
 
         //todo: what if there are no rows? let the user know;
         //todo: escape xss attack with htmlentities();
-    print("
-        <tr>
-            <td><a href='" . route('/index.php?evenementen=wijzigen&evenement_id="'.$id.'"') . "'>$titel</a></td>
-            <td>$onderwerp</td>
-            <td>$datum</td>
-            <td>$adres</td>
-            <td>$locatie</td>
-        </tr>
-");}
-    ?>
-    </table>
-</div>
+        print("
+                <tr>
+                    <td><a href='" . route('index.php?evenementen=specifiek&evenement_id=' . $row['id']) . "'>" . $row['titel'] . "</a></td>
+                    <td>" . $row["onderwerp"] . "</td>
+                    <td>" . $row["datum"] . "</td>
+                    <td>$adres</td>
+                    <td>$locatie</td>
+            </tr>
+            ");
+    }
+    print("</table></div>");
+}else{
+    print("Er zijn geen evenementen op dit moment");
+}
+
+?>
+
