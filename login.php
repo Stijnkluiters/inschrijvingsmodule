33b5f3021265ef4e79e5d6f2ac1ead9b2a8e1d1c
stijn-PC\stijn <stijnkluiters@hotmail.com>
2017-12-11 11:38:52 +0100

    - added mail functionality for the importing of students

--- login.php
+++ login.php
@@ -52,7 +52,15 @@ if( isset($_POST[ 'submit' ]) )
             else
             {
                 //alle inloggegevens kloppen, gebruiker is nu ingelogd. tijd om door te sturen naar het dashboard.
-                redirect('/index.php');
+
+
+                $role = get_account_his_role($_SESSION[authenticationSessionName]);
+                if($role['rolnaam'] === 'leerling') {
+                    redirect('/student/index.php');
+                } else {
+                    redirect('/index.php');
+                }
+
             }
 
         }
