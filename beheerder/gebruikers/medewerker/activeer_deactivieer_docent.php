0bf9cced799e426e3b14b8752593ad399a28fca4
stijnkluiters <stijnkluiters@hotmail.com>
2017-12-21 09:59:47 +0100

    - bugfix: medewerker

--- beheerder/gebruikers/medewerker/activeer_deactivieer_docent.php
+++ beheerder/gebruikers/medewerker/activeer_deactivieer_docent.php
@@ -5,7 +5,7 @@
  * Date: 30/11/2017
  * Time: 12:05
  */
-$afkorting = filter_var(filter_input(INPUT_GET,'afkorting',FILTER_SANITIZE_STRING),FILTER_VALIDATE_INT);;
+$afkorting = filter_input(INPUT_GET,'afkorting',FILTER_SANITIZE_STRING);
 
 $db = db();
 $docentQuery = $db->prepare('SELECT * FROM medewerker WHERE afkorting = :afkorting');
@@ -21,7 +21,7 @@ if($docent['deleted'] == false){
 
     $stmt->bindParam('afkorting', $afkorting, PDO::PARAM_STR);
     $stmt->execute();
-    redirect('/index.php?gebruiker=overzichtmedewerker', $docent['roepnaam'].' is geactiveerd');
+    redirect('/index.php?gebruiker=overzichtmedewerker', $docent['roepnaam'].' is gedeactiveerd');
 } else {
     $stmt = $db->prepare('
             UPDATE medewerker SET
@@ -30,7 +30,7 @@ if($docent['deleted'] == false){
 
     $stmt->bindParam('afkorting', $afkorting, PDO::PARAM_STR);
     $stmt->execute();
-    redirect('/index.php?gebruiker=overzichtmedewerker' , $docent['roepnaam'] . ' is gedeactiveerd');
+    redirect('/index.php?gebruiker=overzichtmedewerker' , $docent['roepnaam'] . ' is geactiveerd');
 }
 
 ?>
